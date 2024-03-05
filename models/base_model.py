54366777b0994aef3a3874caff1d88d53bdb2704
Yahiamostafa11 <yahiawork@gmail.com>
2024-03-05 21:55:47 +0200

    'satamony'

--- models/base_model.py
+++ models/base_model.py
@@ -2,23 +2,32 @@
 """Module for BaseModel class by satamony"""
 import uuid
 from datetime import datetime
+import json
+
 
 class BaseModel:
-    """BaseModel class"""
-    def __init__(self):
+    def __init__(self, name, my_number):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.name = name
+        self.my_number = my_number
+        self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return '[BaseModel] ({}) {}'.format(self.id, self.to_dict())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'my_number': self.my_number,
+            '__class__': self.__class__.__name__,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        }
 
     def save(self):
         self.updated_at = datetime.now()
 
-    def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+    def to_json(self):
+        return json.dumps(self.to_dict(), default=str)
+
