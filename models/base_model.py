2c229ad66d5086132ac38a700e33de7fec12d6ed
Yahiamostafa11 <yahiawork@gmail.com>
2024-03-06 22:08:19 +0200

    'stamony'

--- models/base_model.py
+++ models/base_model.py
@@ -2,15 +2,35 @@
 """Module for BaseModel class by satamony"""
 import uuid
 from datetime import datetime
-import json
+
 
 
 class BaseModel:
-    def __init__(self, name, usrnumber):
-        self.id = str(uuid.uuid4())
-        self.name = name
-        self.usrnumber = usrnumber
-        self.created_at = self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if args:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                elif i == 1:
+                    self.created_at = datetime.strptime(args[i], '%Y-%m-%dT%H:%M:%S.%f')
+                elif i == 2:
+                    self.updated_at = datetime.strptime(args[i], '%Y-%m-%dT%H:%M:%S.%f')
+                elif i == 3:
+                    self.name = args[i]
+                elif i == 4:
+                    self.usrnumber = args[i]
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
     def __str__(self):
         return '[BaseModel] ({}) {}'.format(self.id, self.to_dict())
@@ -28,6 +48,5 @@ class BaseModel:
     def save(self):
         self.updated_at = datetime.now()
 
-    def to_json(self):
-        return json.dumps(self.to_dict(), default=str)
+    
 
